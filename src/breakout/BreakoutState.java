afde3a8583c13051924c9bdda3e1fe81df6c824d
Snowlet <snowlettv@gmail.com>
2022-05-15 16:27:11 +0200

    Correct Breakoutclass

--- src/breakout/BreakoutState.java
+++ src/breakout/BreakoutState.java
@@ -1,22 +1,8 @@
 package breakout;
 
-import utils.Vector;
-
-public class BreakoutState {
-	
-	private static final Vector PADDLE_VEL = new Vector(20, 0);
-	public static final int MAX_BALL_REPLICATE = 5;
-	private static final Vector[] BALL_VEL_VARIATIONS = new Vector[] { new Vector(0, 0), new Vector(2, -2),
-			new Vector(-2, 2), new Vector(2, 2), new Vector(-2, -2) };
-	public static int MAX_ELAPSED_TIME = 50;
-
-}
-
-package breakout;
-
-		import java.util.ArrayList;
-		import java.util.Arrays;
-		import java.util.Objects;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
 
 /**
  * Represents the current state of a breakout game.
